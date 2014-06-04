47f3e39e18ae81fc0e90916a59319a0be61e91e7
SinnerA <wu181184@sina.com>
2014-06-04 20:49:48 +0800

    BP_Examples

---
+++ chapter 7(more abstract)/muffledException.py
@@ -0,0 +1,18 @@
+class MuffluedCalculator:
+    muffled = False
+    def calc(self, expr):
+        try:
+            return eval(expr)
+        except ZeroDivisionError:
+            if self.muffled:
+                print "Division by zero is illegal!"
+            else:
+                raise
+
+calculator = MuffluedCalculator()
+print calculator.calc('10/2')
+
+#calculator.calc('10/0')
+
+calculator.muffled = True
+calculator.calc('10/0')
